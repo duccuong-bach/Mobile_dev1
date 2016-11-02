40b879ae0b61a773bab0158b51486de14df54697
kezhen-tan <kezhen-tan@cubesystem.co.jp>
2016-11-02 20:54:21 +0900

    テスト
    編集しました

--- app/src/main/java/cubesystem/kurimabank/activity/Summary.java
+++
@@ -1,7 +0,0 @@
-package cubesystem.kurimabank.activity;
-
-/**
- * Created by sayaka-kurima on 2016/09/07.
- */
-public class Summary {
-}
--- app/src/main/java/cubesystem/kurimabank/model/Action.java
+++
@@ -1,9 +0,0 @@
-package cubesystem.kurimabank.model;
-
-/**
- * Created by kurimabank-kurima on 2016/03/22.
- */
-public interface Action {
-
-    public String getSummary();
-}
